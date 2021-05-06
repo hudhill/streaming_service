f7baa8c980223b87fc7b29d7a099f0b60d7682b2
hudhill <sam.hudhill@gmail.com>
2021-05-06 19:53:50 +0100

    setup

---
+++ src/movie.py
@@ -0,0 +1,6 @@
+class Movie:
+
+    def __init__(self, title, director, rating):
+        self.title = title 
+        self.rating = rating
+        self.director = director
---
+++ src/profile.py
@@ -0,0 +1,6 @@
+class Profle:
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.favourites = []
